1640a108f861141c0751640b4b6bdcb1c3bd047c
camelCaseDave <dave.clark@hh-lap-278.home>
2021-12-24 12:55:03 +0000

    fixes jpg ending

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,6 +1,6 @@
 import GalleryCard from "./GalleryCard";
 import history from "../public/history.jpg";
-import landscape from "../public/landscape.jpg";
+import landscape from "../public/landscape.JPG";
 import questionable from "../public/questionable.JPG";
 
 export default function Gallery() {
